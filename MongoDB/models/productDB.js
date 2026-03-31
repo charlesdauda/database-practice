493ad2b9aa8f5d07f99eecd8403563572dd6bfd1
CHARLES DAUDA <108020360+charlesdauda@users.noreply.github.com>
2026-03-31 14:17:12 +0000

    Create productDB.js with product schema
    Define product schema for MongoDB with validation rules.

---
+++ MongoDB/models/productDB.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    title: {type: String, required: true, trim: true},
+    description: {type: String, required: true, trim: true},
+    price: {type: mongoose.Decimal128, 
+        required: true, min: 0
+    },
+    stock: {type: Number, required: true, min: 0},
+    category: {type: String, required: true, trim: true},
+    tags: {type: Array},
+    isAvailable: {type: Boolean, default: true},
+    reviews: [{reviewer: {type: String, required: true}, 
+        comment: {type: String, required: true}, 
+        rating: {type: Number, min: 1, max:5}}],
+    createdAt: {type: Date, default: Date.now},
+    publishedAt: {type: Date, default: Date.now}
+})
