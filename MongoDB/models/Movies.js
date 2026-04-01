cf8c11c466396409788b0b1bcc6bdfc7c51b9241
CHARLES DAUDA <108020360+charlesdauda@users.noreply.github.com>
2026-04-01 17:39:16 +0000

    Implement createDoc function for movie model
    Added a function to create and save a new movie document.

--- MongoDB/models/Movies.js
+++ MongoDB/models/Movies.js
@@ -14,5 +14,27 @@ const movieSchema = new mongoose.Schema({ //also works with const movieSchema = 
     comments: [{value: {type: String}, published: {type: Date, default: Date.now}}]
 })
 
-const movieModel = mongoose.model("movie", movieSchema);
-export default movieModel;
+//creating model 
+const MovieModel = mongoose.model('movie', movieSchema);
+
+const createDoc = async () => {
+    try {
+        //creating new document 
+       const m1 = new MovieModel({
+            name: "Extraction",
+            ratings: 3,
+            money: 6000,
+            genre: ['action', 'adventure'],
+            isActive: true,
+            comments: [{value: "That was Amazing"}]
+        })
+
+        const result = await m1.save(); 
+        console.log(result);
+
+    }catch(error){
+        console.log(error);
+    }
+}
+
+export { createDoc };
