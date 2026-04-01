b0238c05b55d0aaf3f77190d5c0251943a92e957
CHARLES DAUDA <108020360+charlesdauda@users.noreply.github.com>
2026-04-01 17:33:09 +0000

    Integrate document creation for movies and products
    Added function calls to create documents for movies and products.

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import connectDB from "./db/connectDB.js";
-import movieModel from "./models/movies.js";
-import productModel from "./models/Product.js";
+import { createDoc } from "./models/movies.js";
+import { productDoc } from "./models/Product.js";
 
 const app = express(); //Creating a server app with express
 const port = process.env.PORT || 8000; //run on a server or port 8000
 const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/movies'; //if .env has DATABASE_URL, use it else use local mongodb
 
 connectDB(DATABASE_URL); //connecting mongoose to mongodb
+createDoc(); 
+productDoc();
 
 app.listen(port, () => console.log(`Server listening on port ${port}`)); // Starts the express server
+
+
